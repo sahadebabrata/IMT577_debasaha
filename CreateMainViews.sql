63d830e84dd8b5549ea27f71b20d0da872c020a0
Debabrata Saha <95506759+sahadebabrata@users.noreply.github.com>
2022-05-22 17:13:52 -0700

    Assignment 8 - Final Data Warehouse Submission

--- CreateMainViews.sql
+++ CreateMainViews.sql
@@ -1,6 +1,5 @@
-
 --VIEW FOR STORE-CHANNEL TARGETS 
-CREATE OR REPLACE VIEW VW_STORE_SALES_TARGET AS
+CREATE OR REPLACE SECURE VIEW VW_STORE_SALES_TARGET AS
 SELECT
 	ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" , dd."YEAR" , SUM(fsst."SalesTargetAmount") AS "StoreSalesTargetAmount"
 	--ChannelID is necessary since we are measuring Store sales, which means we need to exclude Reseller targets 
@@ -14,11 +13,9 @@ WHERE
 	AND ds."StoreNumber" IN ('5', '8')
 	AND dd.DATE_PKEY = fsst."DimTargetDateID" 
 GROUP BY ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" , dd."YEAR";
---ORDER BY dd."YEAR" , ds."StoreNumber" ;
-SELECT * FROM VW_STORE_SALES_TARGET; 
 
 -- VIEW FOR AGGREGATE SALES DATA  
-CREATE OR REPLACE VIEW VW_STORE_AGG_SALES_DATA AS
+CREATE OR REPLACE SECURE VIEW VW_STORE_AGG_SALES_DATA AS
 SELECT  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" , dd.YEAR, 
 SUM(fsa."SaleQuantity") AS "TotalSaleQuantity", SUM(fsa."SaleAmount") AS "TotalSaleAmount", SUM(fsa."SaleExtendedCost") AS "TotalSaleExtendedCost", 
 SUM(fsa."SaleTotalProfit") AS "TotalSaleProfit"
@@ -29,24 +26,21 @@ AND dp."DimProductID" = fsa."DimProductID"
 AND dc."DimChannelID" = fsa."DimChannelID" 
 AND ds."StoreNumber" IN ('5', '8')
 GROUP BY  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" , dd.YEAR;
---
-SELECT * FROM VW_STORE_AGG_SALES_DATA ; 
+
 
 
 ---VIEW FOR PRODUCT SALES TARGET
-CREATE OR REPLACE VIEW VW_STORE_PRODUCT_SALES_TARGET AS
+CREATE OR REPLACE SECURE VIEW VW_STORE_PRODUCT_SALES_TARGET AS
 SELECT dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory", dd."YEAR" , SUM(fpst."ProductTargetSalesQuanity") AS "TotalProductSalesTargetQuantity"
 FROM FACT_PRODUCTSALESTARGET fpst, DIM_PRODUCT dp, DIM_DATE dd
 WHERE dp."DimProductID" = fpst."DimProductID" 
 AND dd.DATE_PKEY = fpst."DimTargetDateID" 
 GROUP BY dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory", dd."YEAR" 
 ORDER BY dd."YEAR" ;
---
-SELECT * FROM VW_STORE_PRODUCT_SALES_TARGET; 
 
 
 -- VIEW FOR PRODUCT AGGREGATE SALES BY DAY OF WEEK   
-CREATE OR REPLACE VIEW VW_STORE_AGG_SALES_BY_DAY AS
+CREATE OR REPLACE SECURE VIEW VW_STORE_AGG_SALES_BY_DAY AS
 SELECT  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" , dd.DAY_NUM_IN_WEEK, dd.DAY_NAME , dd.DAY_ABBREV , 
 SUM(fsa."SaleQuantity") AS "TotalSaleQuantity", SUM(fsa."SaleAmount") AS "TotalSaleAmount", SUM(fsa."SaleExtendedCost") AS "TotalSaleExtendedCost", 
 SUM(fsa."SaleTotalProfit") AS "TotalSaleProfit"
@@ -58,12 +52,10 @@ AND dc."DimChannelID" = fsa."DimChannelID"
 AND ds."StoreNumber" IN ('5', '8')
 GROUP BY  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , 
 dc."SourceChannelID" , dc."ChannelName" , dd.DAY_NUM_IN_WEEK, dd.DAY_NAME , dd.DAY_ABBREV;
---
-SELECT * FROM VW_STORE_AGG_SALES_BY_DAY; 
 
 
 -- VIEW FOR PRODUCT SALES DAILY
-CREATE OR REPLACE VIEW VW_STORE_SALES_DAILY AS
+CREATE OR REPLACE SECURE VIEW VW_STORE_SALES_DAILY AS
 SELECT  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , dc."SourceChannelID" , dc."ChannelName" ,
 dd."DATE", dd.FULL_DATE_DESC, dd.DAY_NUM_IN_WEEK, dd.DAY_NUM_IN_MONTH, dd.DAY_NUM_IN_YEAR, dd.DAY_NAME, dd.DAY_ABBREV, dd.WEEKDAY_IND, dd.US_HOLIDAY_IND, dd."_HOLIDAY_IND", dd.MONTH_END_IND, dd.WEEK_BEGIN_DATE_NKEY, dd.WEEK_BEGIN_DATE, dd.WEEK_END_DATE_NKEY, dd.WEEK_END_DATE, dd.WEEK_NUM_IN_YEAR, dd.MONTH_NAME, dd.MONTH_ABBREV, dd.MONTH_NUM_IN_YEAR, dd.YEARMONTH, dd.QUARTER, dd.YEARQUARTER, dd."YEAR", dd.FISCAL_WEEK_NUM, dd.FISCAL_MONTH_NUM, dd.FISCAL_YEARMONTH, dd.FISCAL_QUARTER, dd.FISCAL_YEARQUARTER, dd.FISCAL_HALFYEAR, dd.FISCAL_YEAR ,
 SUM(fsa."SaleQuantity") AS "TotalSaleQuantity", SUM(fsa."SaleAmount") AS "TotalSaleAmount", SUM(fsa."SaleExtendedCost") AS "TotalSaleExtendedCost", 
@@ -76,5 +68,3 @@ AND dc."DimChannelID" = fsa."DimChannelID"
 AND ds."StoreNumber" IN ('5', '8')
 GROUP BY  dp."SourceProductID" , dp."ProductName", dp."SourceProductCategoryID" , dp."ProductCategory" , ds."StoreNumber" , 
 dc."SourceChannelID" , dc."ChannelName" , dd."DATE", dd.FULL_DATE_DESC, dd.DAY_NUM_IN_WEEK, dd.DAY_NUM_IN_MONTH, dd.DAY_NUM_IN_YEAR, dd.DAY_NAME, dd.DAY_ABBREV, dd.WEEKDAY_IND, dd.US_HOLIDAY_IND, dd."_HOLIDAY_IND", dd.MONTH_END_IND, dd.WEEK_BEGIN_DATE_NKEY, dd.WEEK_BEGIN_DATE, dd.WEEK_END_DATE_NKEY, dd.WEEK_END_DATE, dd.WEEK_NUM_IN_YEAR, dd.MONTH_NAME, dd.MONTH_ABBREV, dd.MONTH_NUM_IN_YEAR, dd.YEARMONTH, dd.QUARTER, dd.YEARQUARTER, dd."YEAR", dd.FISCAL_WEEK_NUM, dd.FISCAL_MONTH_NUM, dd.FISCAL_YEARMONTH, dd.FISCAL_QUARTER, dd.FISCAL_YEARQUARTER, dd.FISCAL_HALFYEAR, dd.FISCAL_YEAR;
---
-SELECT * FROM VW_STORE_SALES_DAILY; 
